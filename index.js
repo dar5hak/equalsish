7dcdac1d7e2012090a7346ef764ea07f62b1f835
Darshak Parikh <dar5hak@users.noreply.github.com>
2016-06-16 12:32:35 +0530

    Remove parentheses from around arrow function arg

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function equalsish(stringOne, stringTwo) {
 	if (stringOne === undefined) {
 		throw new Error('No arguments provided.');
 	} else if (stringTwo === undefined) {
-		return (futureString) => equalsish(stringOne, futureString);
+		return futureString => equalsish(stringOne, futureString);
 	} else {
 		return naked(stringOne) === naked(stringTwo);
 	}
